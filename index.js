7e6f0a025114d6cdca297e9dc009a685231529c9
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-08-30 17:26:56 -0400

    updated index and shapes js

--- index.js
+++ index.js
@@ -1,7 +1,55 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-const shapes = require ("./lib/shapes");
+import inquirer from 'inquirer';
 
-const shape = new shapes();
+const { Triangle, Circle, Square } = require("./lib/shapes");
+const { type } = require("os");
 
-shape.run();
+async function generateLogo() {
+  const userInput = await inquirer.prompt([
+    // Define prompts for color, shape, and text
+
+    {
+      name: "shape",
+      type: "list",
+      message: "Select a color:",
+      choices: ["Triangle", "Circle", "Square"],
+    },
+
+    {
+      name: "text",
+      type: "input",
+      message: "Enter the logo text (no longer then 3 characters): ",
+    },
+
+    {
+      name: "fillColor",
+      type: "input",
+      message: "Enter logo background color: ",
+    },
+
+    {
+      name: "textColor",
+      type: "input",
+      message: "Enter text color: ",
+    },
+  ]);
+
+  let selectedShape;
+  if (userInput.shape === "Triangle") {
+    selectedShape = new Triangle();
+  } else if (userInput.shape === "Circle") {
+    selectedShape = new Circle();
+  } else if (userInput.shape === "Square") {
+    selectedShape = new Square();
+  }
+
+  selectedShape.setColor(userInput.fillColor);
+  selectedShape.setText(userInput.text);
+
+  const svg = selectedShape.generateSVG();
+
+  fs.writeFileSync("generatedLogo.svg", svg);
+}
+
+generateLogo();
