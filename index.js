9e2749ce523dc33b8d78acef5c8dea9143fa75b3
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-09-07 16:19:51 -0400

    progress on shapes class and index js funtions

--- index.js
+++ index.js
@@ -1,55 +1,83 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
-import inquirer from 'inquirer';
-
 const { Triangle, Circle, Square } = require("./lib/shapes");
-const { type } = require("os");
+const { log } = require("console");
+
+const userInput = inquirer.prompt([
+  // Define prompts for color, shape, and text
+  {
+    name: "text",
+    type: "input",
+    message: "Enter the logo text (no longer the 3 characters): ",
+    validate: (input) => input.length <= 3,
+  },
+  {
+    name: "shape",
+    type: "list",
+    message: "Select a shape:",
+    choices: ["Triangle", "Circle", "Square"],
+  },
+  {
+    name: "shapeColor",
+    type: "input",
+    message: "Enter shape color: ",
+  },
+  {
+    name: "textColor",
+    type: "input",
+    message: "Enter text color: ",
+  },
+]);
+
+// Example writeFile function (as provided in the previous answer)
+function writeFile(fileName, data) {
+  FileSystem.writeFile(fileName, data, (err) => {
+    if (err) {
+      console.error(`Error writing to ${fileName}: ${err}`);
+    } else {
+      console.log(`Successfully wrote to ${fileName}`);
+    }
+  });
+}
 
 async function generateLogo() {
-  const userInput = await inquirer.prompt([
-    // Define prompts for color, shape, and text
+  const userResponse = await inquirer.prompt(userInput);
 
-    {
-      name: "shape",
-      type: "list",
-      message: "Select a color:",
-      choices: ["Triangle", "Circle", "Square"],
-    },
+  const userText = userInput.text;
+  // console.log(userText);
+  const userTextColor = userInput.userTextColor;
+  // console.log(userTextColor);
+  const userShapeColor = userInput.userShapeColor;
+  // console.log(userShapeColor);
+  const userShapeType = userInput.shape;
 
-    {
-      name: "text",
-      type: "input",
-      message: "Enter the logo text (no longer then 3 characters): ",
-    },
+  let createdShape;
+  if (userShapeType === "Square") {
+    createdShape = new Square();
+    console.log("Square selected ");
 
-    {
-      name: "fillColor",
-      type: "input",
-      message: "Enter logo background color: ",
-    },
+  } else if (userShapeType === "Triangle") {
+    createdShape = new Triangle();
+    console.log("Triangle selected ");
 
-    {
-      name: "textColor",
-      type: "input",
-      message: "Enter text color: ",
-    },
-  ]);
+  } else if (userShapeType === "Circle") {
+    createdShape = new Circle();
+    console.log("Circle selected ");
 
-  let selectedShape;
-  if (userInput.shape === "Triangle") {
-    selectedShape = new Triangle();
-  } else if (userInput.shape === "Circle") {
-    selectedShape = new Circle();
-  } else if (userInput.shape === "Square") {
-    selectedShape = new Square();
+  } else {
+    console.log("Error selecting shape!");
   }
+  
 
-  selectedShape.setColor(userInput.fillColor);
-  selectedShape.setText(userInput.text);
+  
+  // Generate the logo data based on userInput (replace this with your actual logo generation logic)
+  const logoData = generateLogoData(userInput);
 
-  const svg = selectedShape.generateSVG();
+  // Define the file name for the logo
+  const logoFile = "logo.svg";
 
-  fs.writeFileSync("generatedLogo.svg", svg);
+  // Write the logo data to the file
+  writeFile(logoFile, logoData);
 }
 
 generateLogo();
