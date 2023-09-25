8a9dc86ea1d9dfe7718900be4cbf29df98711975
Otabek0111 <134247761+Otabek0111@users.noreply.github.com>
2023-09-25 16:32:47 -0400

    generating shapes

--- index.js
+++ index.js
@@ -1,21 +1,20 @@
 const fs = require("fs");
 const inquirer = require("inquirer");
 const { Triangle, Circle, Square } = require("./lib/shapes");
-const { log } = require("console");
 
-const userInput = inquirer.prompt([
-  // Define prompts for color, shape, and text
-  {
-    name: "text",
-    type: "input",
-    message: "Enter the logo text (no longer the 3 characters): ",
-    validate: (input) => input.length <= 3,
-  },
+const questionInputs = [
+  // prompts to select colors, shape, and text
   {
     name: "shape",
     type: "list",
     message: "Select a shape:",
     choices: ["Triangle", "Circle", "Square"],
+  },
+  {
+    name: "text",
+    type: "input",
+    message: "Enter the logo text (no longer the 3 characters): ",
+    validate: (input) => input.length <= 3,
   },
   {
     name: "shapeColor",
@@ -27,57 +26,56 @@ const userInput = inquirer.prompt([
     type: "input",
     message: "Enter text color: ",
   },
-]);
+];
 
 // Example writeFile function (as provided in the previous answer)
 function writeFile(fileName, data) {
-  FileSystem.writeFile(fileName, data, (err) => {
+  fs.writeFile(fileName, data, (err) => {
     if (err) {
       console.error(`Error writing to ${fileName}: ${err}`);
     } else {
-      console.log(`Successfully wrote to ${fileName}`);
+      console.log(`Successfully created to ${fileName}`);
     }
   });
 }
 
+// Define the file name for the logo
+const logoFile = "logo.svg";
+
 async function generateLogo() {
-  const userResponse = await inquirer.prompt(userInput);
+  const userInput = await inquirer.prompt(questionInputs);
 
   const userText = userInput.text;
-  // console.log(userText);
-  const userTextColor = userInput.userTextColor;
-  // console.log(userTextColor);
-  const userShapeColor = userInput.userShapeColor;
-  // console.log(userShapeColor);
+  const userTextColor = userInput.textColor;
+  const userShapeColor = userInput.shapeColor;
   const userShapeType = userInput.shape;
 
   let createdShape;
   if (userShapeType === "Square") {
-    createdShape = new Square();
+    createdShape = new Square(userShapeColor, userTextColor, userText);
     console.log("Square selected ");
-
   } else if (userShapeType === "Triangle") {
-    createdShape = new Triangle();
+    createdShape = new Triangle(userShapeColor, userTextColor, userText);
     console.log("Triangle selected ");
-
   } else if (userShapeType === "Circle") {
-    createdShape = new Circle();
+    createdShape = new Circle(userShapeColor, userTextColor, userText);
     console.log("Circle selected ");
-
   } else {
     console.log("Error selecting shape!");
   }
-  
+  console.log("Shape Color: " + userShapeColor);
+  console.log("Text: " + userText);
+  console.log("Text Color: " + userTextColor);
 
-  
-  // Generate the logo data based on userInput (replace this with your actual logo generation logic)
-  const logoData = generateLogoData(userInput);
+  createdShape.shapeColor = userShapeColor;
+  createdShape.textColor = userTextColor;
+  createdShape.text = userText;
 
-  // Define the file name for the logo
-  const logoFile = "logo.svg";
+
+  const svgString = createdShape.render();
 
   // Write the logo data to the file
-  writeFile(logoFile, logoData);
+  writeFile(logoFile, svgString);
 }
 
 generateLogo();
